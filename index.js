851a17f29ba6333bf554bd0bb3fa235b228a39fb
Marie G <mari09A7@stud.kea.dkgit config --global user.email mari09A7@stud.kea.dk>
2022-01-10 20:14:20 +0100

    10/1

--- index.js
+++ index.js
@@ -37,3 +37,24 @@ function tilbage() {
 }
 
 btn1.addEventListener("click", tilbage);
+
+//-------------------------------------------------------tilbage til top--------------------------------------------------------------//
+//Get the button:
+mybutton = document.getElementById("myBtn");
+
+// når man scroller ned 20px fra toppen af dokumentet, hvis knappen
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// når man klikker på knapper scroller den op
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
